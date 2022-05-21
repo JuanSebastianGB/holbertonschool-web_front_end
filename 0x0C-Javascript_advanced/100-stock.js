3295f27a7db54f44f2de8f10aaf4a1089fd7d0ab
JuanSebastianGB <3393@holbertonschool.com>
2022-05-20 23:01:28 -0500

    Advanced task completed

---
+++ 0x0C-Javascript_advanced/100-stock.js
@@ -0,0 +1,24 @@
+
+const stock = {
+  macbook: 2,
+  iphone: 4
+};
+
+const processPayment = (itemName) => {
+  stock[itemName] -= 1;
+  console.log(`Payment is being processed for item ${itemName}`);
+};
+
+const processError = (itemName) => {
+  console.log(`No more ${itemName} in stock`);
+  console.log('Payment is not being processed');
+};
+
+const processOrder = (itemName, callbackPayment, callbackError) => {
+  console.log(`Verifying the stock of ${itemName}`);
+  const item = itemName.toLowerCase();
+  if (stock[item] > 0) { callbackPayment(item); } else { callbackError(item); }
+};
+
+const item = prompt('Please enter the item you would like to purchase (Macbook, iPhone)');
+processOrder(item, processPayment, processError);

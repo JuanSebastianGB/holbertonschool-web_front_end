2069080359c62ad9dee8bc7ce297e38e0babe4a2
JuanSebastianGB <3393@holbertonschool.com>
2022-05-24 21:39:28 -0500

    FInishing task 11

--- 0x0D-JQuery_advanced/11-index.js
+++ 0x0D-JQuery_advanced/11-index.js
@@ -1,3 +1,4 @@
+const API_URL = 'http://localhost:3000/posts';
 const addPostRow = (data) => {
   const { id, title, author } = data;
   const paragraph = `<p>Post created with id ${id}, title: ${title}, author: ${author}</p>`;
@@ -5,32 +6,20 @@ const addPostRow = (data) => {
 };
 
 const listPosts = async () => {
-  const request = await $.get('http://localhost:3000/posts');
+  const request = await $.get(API_URL);
   request.map((element) => addPostRow(element));
-};
-
-const sendForm = () => {
-  $('form').submit(function (e) {
-    e.preventDefault();
-    const message = '<p>About to send the query to the API</p>';
-    $(this).after(message);
-    const data = new FormData(document.querySelector('form'));
-    const newData = [...data];
-    console.log(data);
-    console.log(newData);
-  });
 };
 
 const buildForm = () => {
   let form = `
-    <form>
+    <form class='form'>
       <div>
         <label for="author">Author</label>
-        <input type="text" id="author">
+        <input type="text" id="author" name="author">
       </div>
       <div>
         <label for="title">Title</label>
-        <textarea id="title" cols="12" rows="2"></textarea>
+        <textarea id="title" name="title" cols="12" rows="2"></textarea>
       </div>
       <input type="submit" value="Submit">
     </form>
@@ -40,6 +29,25 @@ const buildForm = () => {
 
   sendForm();
 };
+
+function sendForm() {
+  $('.form').submit(async function (e) {
+    const message = '<p>About to send the query to the API</p>';
+    $('.form').after(message);
+    let data = $(this).serializeArray();
+    const dataObject = Object.fromEntries(
+      data.map((element) => [element.name, element.value])
+    );
+    try {
+      const response = await $.post(API_URL, dataObject);
+      if (response) addPostRow(dataObject);
+    } catch (error) {
+      alert('Error sending the POST query');
+    }
+
+    e.preventDefault();
+  });
+}
 $(document).ready(function () {
   listPosts();
   buildForm();
